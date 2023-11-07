0d8a9aec6c3b35ce28bc0901de80915c11b490a2
Joe Noei <75776501+limingw4159@users.noreply.github.com>
2023-11-07 12:35:57 +1100

    add margin for List

--- src/components/ServiceList.tsx
+++ src/components/ServiceList.tsx
@@ -8,11 +8,10 @@ import Typography from "@mui/material/Typography";
 import CircularProgress from "@mui/material/CircularProgress";
 import Box from "@mui/material/Box";
 import Alert from "@mui/material/Alert";
-import Card from "@mui/material/Card";
-import CardContent from "@mui/material/CardContent";
 import Avatar from "@mui/material/Avatar";
 import { blue } from "@mui/material/colors";
 import ListItemAvatar from "@mui/material/ListItemAvatar";
+import Paper from "@mui/material/Paper";
 
 const ServiceList: React.FC = () => {
   const { data, isLoading, error } = useFetch<Service[]>("");
@@ -41,23 +40,32 @@ const ServiceList: React.FC = () => {
   return (
     <List sx={{ width: "100%" }}>
       {data?.map((service, index) => (
-        <ListItem key={index} alignItems="flex-start">
-          <ListItemAvatar>
-            <Avatar sx={{ bgcolor: blue[500] }}></Avatar>
-          </ListItemAvatar>
-          <ListItemText
-            primary={
-              <Typography variant="h6" component="div">
-                {service.name}
-              </Typography>
-            }
-            secondary={
-              <Typography variant="body2" color="text.secondary">
-                {service.version}
-              </Typography>
-            }
-          />
-        </ListItem>
+        <Paper
+          key={index}
+          elevation={1}
+          sx={{ my: 1, mx: 2, "&:hover": { boxShadow: 6 } }}
+        >
+          <ListItem
+            alignItems="flex-start"
+            sx={{ "&:hover": { bgcolor: "action.hover" }, marginBottom: 3 }}
+          >
+            <ListItemAvatar>
+              <Avatar sx={{ bgcolor: blue[500] }}></Avatar>
+            </ListItemAvatar>
+            <ListItemText
+              primary={
+                <Typography variant="h6" component="div">
+                  {service.name}
+                </Typography>
+              }
+              secondary={
+                <Typography variant="body2" color="text.secondary">
+                  {service.version}
+                </Typography>
+              }
+            />
+          </ListItem>
+        </Paper>
       ))}
     </List>
   );
